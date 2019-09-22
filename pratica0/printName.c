6e7e7e2ce687e0a0d2cd020e0443397cd64ea521
diegoviniciusit <diegoviniciusit@gmail.com>
2019-09-21 21:10:38 -0300

    Add files via upload

---
+++ pratica0/printName.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[]) {
+	printf("Diego Vinicius de Oliveira Silva\n");
+	return 0;
+}
---
+++ pratica0/sen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+void main ()
+{
+	double seno;
+	seno = sin(3.14);
+	double seno2;
+	seno2 = sin(4.13);
+	printf("Seno de 3.14 é %f\n", seno);
+	printf("Seno de 4.13 é %f\n", seno2);
+}
